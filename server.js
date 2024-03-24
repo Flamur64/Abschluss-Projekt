d9ba54bf4841318696a928ed64ccb4572bcfaa7a
Flamur64 <flamurim64@gmail.com>
2024-03-24 13:07:56 +0100

    fixed eslint issues

--- server.js
+++ server.js
@@ -8,9 +8,10 @@ const fs = require('fs');
 
 require('dotenv').config();
 
+
 const s3 = new AWS.S3({
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  // accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  // secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   region: 'eu-central-1'
 });
 
