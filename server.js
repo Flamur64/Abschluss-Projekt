b2bb4abc7e16f0f31893c9a53553ce3951c87b8e
Flamur64 <flamurim64@gmail.com>
2024-03-24 18:52:21 +0100

    pipeline fix

--- server.js
+++ server.js
@@ -10,8 +10,8 @@ require('dotenv').config();
 
 // AWS Infomrationen
 const s3 = new AWS.S3({
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+ // accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+ // secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   region: 'eu-central-1'
 });
 
